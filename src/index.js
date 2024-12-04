70b5ecfdfea3d9614bcc413151b060ebc5f87b39
didier chavez <didierchavez2101@gmail.com>
2024-12-04 15:14:45 -0500

    first commit8

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: "sistema-inventario-frontend-lxxvg337a.vercel.app",
+    origin: "sistema-inventario-frontend-dusky.vercel.app",
   })
 );
 
